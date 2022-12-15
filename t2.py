f776f28a69204a0564fc3d4a5bd05feddf8a222a
Vishesh Handa <me@vhanda.in>
2022-12-15 18:45:14 +0530

    Avoid hard coding the shape of the output
    We now allow far more kinds of input. Earlier only 3 parameters were
    customizable, and now it takes some 8 parameters.

--- t2.py
+++ t2.py
@@ -4,21 +4,13 @@ from beancount.core.number import Decimal
 from beancount.parser import printer
 
 import beancount.core.data
+import beancount.loader
 import itertools
+import copy
 
 from datetime import date, timedelta
 
 cp = beancount.core.data.create_simple_posting
-
-input_str = "44715.0,44715.0,BIZUM ENVIADO,,-30.0,2097.06"
-
-parts = input_str.split(",")
-parts = [x for x in parts if x != ""]
-parts = list(dict.fromkeys(parts))
-
-# Not sure if empty strings and duplicates should be removed
-
-print(parts)
 
 
 def is_str(x):
@@ -33,12 +25,24 @@ def is_num(x):
         return False
 
 
+def is_iso_date(x):
+    try:
+        date.fromisoformat(x)
+        return True
+    except:
+        return False
+
+
 def is_date(x):
+    return is_excel_date(x) or is_iso_date(x)
+
+
+def is_excel_date(x):
     if not is_num(x):
         return False
 
     x = to_excel_date(float(x))
-    if 2000 <= x.year and x.year <= 2025:
+    if 1900 <= x.year and x.year <= 2100:
         return True
 
     #
@@ -55,7 +59,7 @@ def to_excel_date(excel_date_number):
 
 
 def fetch_fn_indexes(parts, fn):
-    indexes = []
+    indexes = [-1]
     i = 0
     while i < len(parts):
         if fn(parts[i]):
@@ -76,47 +80,6 @@ def fetch_str_i(parts):
     return fetch_fn_indexes(parts, is_str)
 
 
-def fetch_matches(parts):
-    return itertools.product(fetch_date_i(parts), fetch_decimal_i(parts), fetch_str_i(parts))
-
-
-def create_txn(date_arg, str_arg, decimal_arg):
-    assert (isinstance(date_arg, date))
-    assert (isinstance(str_arg, str))
-    assert (isinstance(decimal_arg, Decimal))
-
-    t = Transaction(date=date_arg,
-                    meta={},
-                    postings=[],
-                    flag=flags.FLAG_OKAY,
-                    payee=None,
-                    narration=str_arg,
-                    tags=frozenset(),
-                    links=frozenset())
-    date_arg = Posting(Account('Assets:Personal:Spain:LaCaixa'),
-                       Amount(decimal_arg, "EUR"), None,
-                       None,
-                       None,
-                       None,)
-
-    t.postings.append(date_arg)
-    return printer.format_entry(t)
-
-
-ms = create_txn(date(2022, 6, 3), "BIZUM ENVIADO", Decimal("-30.0"))
-for m in fetch_matches(parts):
-    ts = create_txn(to_excel_date(
-        float(parts[m[0]])), parts[m[2]], Decimal(parts[m[1]]))
-    if ts == ms:
-        print("Matched")
-        print(m)
-        print(ts)
-
-# Python check if type is date
-print()
-print("Expected output")
-print(ms)
-
 """
 Operations -
 string - to_num (with comma as decimal) - float
@@ -125,18 +88,171 @@ num - abs - num
 num - excel_date - Date
 num -
 
-
-State - [ ..... ]
-
-
 2011-02-01 * Credit Interest Capitalised
   Assets:Personal:India:CanaraBank  787.0 INR
 
-Extracted inputs -
-* date
-* payee
-* account name
-* amount
+"""
+
+
+# FIXME: Parse different kinds of dates
+# FIXME: Parts different kinds of numbers to amount
+# FIXME: Different string matching
+# Remove quotes and then case changes
+# Trim spaces
+
+# Figure out how to match the currency
+# def is_amount(x): "EUR 2,34" "2,34 EUR"
 
 
 """
+404-7319078-6347502,3 Scale Home Brew Hydrometer Wine Beer Cider Alcohol Testing Making Tester; ,Vishesh Handa,2019-06-24,"EUR 2,34",N/A,N/A,N/A,N/A,
+
+->
+
+Figure out the inputs -
+1. Assume everything is an input
+
+For this input, create a function that takes a lot of inputs and creates a transaction
+
+"""
+
+
+input_s = """
+2019-06-24 * "3 Scale Home Brew Hydrometer Wine Beer Cider Alcohol Testing Making Tester;"
+  orderId: "404-7319078-6347502"
+  Expenses:Personal:Amazon  -2.34 EUR
+"""
+
+
+def parse_txn(input_s):
+    r = beancount.loader.load_string(input_s)
+
+    txns = r[0]
+    assert (len(txns) == 1)
+    txn = txns[0]
+
+    meta = txn.meta
+    meta.pop("filename")
+    meta.pop("lineno")
+    meta.pop('__tolerances__')
+
+    txn = txn._replace(meta=meta)
+
+    postings = txn.postings
+    assert (len(postings) == 1)
+
+    return txn
+
+
+def fetch_matches(parts):
+    date_args = fetch_date_i(parts)
+    narration_args = fetch_str_i(parts)
+    payee_args = fetch_str_i(parts)
+    meta0_args = fetch_str_i(parts)
+    meta1_args = fetch_str_i(parts)
+    meta2_args = fetch_str_i(parts)
+    posting_units_numbers_args = fetch_decimal_i(parts)
+    posting_units_currency_args = fetch_str_i(parts)
+
+    return itertools.product(date_args, narration_args, payee_args, meta0_args, meta1_args, meta2_args, posting_units_numbers_args, posting_units_currency_args)
+
+
+# 8 inputs
+def build_txn(base_txn: Transaction, date_arg, narration, payee, meta0, meta1, meta2, posting_units_number, posting_units_currency):
+
+    assert (isinstance(date_arg, date))
+    assert (isinstance(narration, str))
+    assert (isinstance(payee, str) or payee is None)
+    assert (isinstance(meta0, str) or meta0 is None)
+    assert (isinstance(meta1, str) or meta1 is None)
+    assert (isinstance(meta2, str) or meta2 is None)
+    assert (isinstance(posting_units_number, Decimal))
+    assert (isinstance(posting_units_currency, str))
+
+    txn = copy.deepcopy(base_txn)
+    txn = txn._replace(date=date_arg)
+    txn = txn._replace(narration=narration)
+    txn = txn._replace(payee=payee)
+
+    meta = txn.meta
+    meta_i = 0
+    for k, v in meta.items():
+        if meta_i == 0:
+            if meta0 is not None:
+                meta[k] = meta0
+            else:
+                break
+        if meta_i == 1:
+            if meta1 is not None:
+                meta[k] = meta1
+            else:
+                break
+
+        if meta_i == 2:
+            if meta2 is not None:
+                meta[k] = meta2
+            else:
+                break
+        meta_i += 1
+    txn = txn._replace(meta=meta)
+
+    assert (len(txn.postings) == 1)
+    posting = txn.postings[0]
+    posting = posting._replace(units=Amount(
+        posting_units_number, posting_units_currency))
+    txn = txn._replace(postings=[posting])
+
+    return txn
+
+
+input_str = "44715.0,44715.0,BIZUM ENVIADO,,-30.0,2097.06"
+
+parts = input_str.split(",")
+# parts = [x for x in parts if x != ""]
+# parts = list(dict.fromkeys(parts))
+parts.append("EUR")
+
+# Not sure if empty strings and duplicates should be removed
+
+print(parts)
+
+
+expected_output = """
+2022-06-03 * "BIZUM ENVIADO"
+  Assets:Personal:Spain:LaCaixa  -30.0 EUR
+""".strip()
+
+base_txn = parse_txn(expected_output)
+
+
+for m in fetch_matches(parts):
+    assert (len(m) == 8)
+
+    date_arg = to_excel_date(float(parts[m[0]])) if m[0] != -1 else None
+    narration_arg = str(parts[m[1]]) if m[1] != -1 else None
+    payee_arg = str(parts[m[2]]) if m[2] != -1 else None
+    meta_0_arg = str(parts[m[3]]) if m[3] != -1 else None
+    meta_1_arg = str(parts[m[4]]) if m[4] != -1 else None
+    meta_2_arg = str(parts[m[5]]) if m[5] != -1 else None
+    posting_units_number = Decimal(parts[m[6]]) if m[6] != -1 else None
+    posting_units_currency = str(parts[m[7]]) if m[7] != -1 else None
+
+    if date_arg == None:
+        continue
+    if narration_arg == None:
+        continue
+    if posting_units_number == None:
+        continue
+    if posting_units_currency == None:
+        continue
+
+    new_txn = build_txn(
+        base_txn, date_arg, narration_arg, payee_arg, meta_0_arg, meta_1_arg, meta_2_arg, posting_units_number, posting_units_currency
+    )
+    actual_output = printer.format_entry(new_txn).strip()
+
+    if actual_output == expected_output:
+        print("Matched")
+        print(actual_output)
+        print(m)
+        break
